bdee74ba08cd308cadcb5886370138939084c039
Carlos Agelvis <carlosarturo.agelvisvergara@connect.qut.edu.au>
2025-08-01 02:17:16 +1000

    Updating verify-token method

--- api/routes/authRoutes.ts
+++ api/routes/authRoutes.ts
@@ -27,7 +27,7 @@ const authRoutes = Router();
 
 authRoutes.post("/sign-up", validate(newUserSchema), createNewUser);
 authRoutes.post("/verify", validate(verifyTokenSchema), verifyEmail);
-authRoutes.post("/verify-token", isAuth, generateVerificationLink);
+authRoutes.get("/verify-token", isAuth, generateVerificationLink);
 authRoutes.post("/sign-in", signIn);
 authRoutes.get("/profile", isAuth, sendProfile);
 // No need to use isAuth middleware as the user will use this endpoint if they are not authenticated (i.e the token is invalid)
